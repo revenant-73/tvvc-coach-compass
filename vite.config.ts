5dce0b1ed3ee0870919e38d9ef7d57c23d7e9a76
TVVC <coach@tvvc.local>
2025-12-17 16:06:16 -0800

    \"Fix base path for GitHub Pages\"

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/constraint-compass/',
+  base: '/tvvc-coach-compass/',
   server: {
     open: true
   }
